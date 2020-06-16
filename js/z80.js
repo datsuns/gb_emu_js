50c1c3f7b2cdc48ee667ba4a5fe9a4d99654ca20
datsuns <the.skeleton7@gmail.com>
2020-06-16 10:40:43 +0900

    add functions written in sample

--- js/z80.js
+++ js/z80.js
@@ -23,5 +23,65 @@ Z80 = {
 		// clocks that takes while last inst
 		m:0,
 		t:0
+	},
+
+	ADDr_e: function() {
+		Z80._r.a += Z80._r.e;
+		Z80._r.f = 0;
+		if(!(Z80._r.a & 255)) Z80._r.f |= 0x80;
+		if(Z80._r.a > 255) Z80._r.f |= 0x10;
+		Z80._r.a &= 255;
+		Z80._r.m = 1; Z80._r.t = 4;
+	}
+
+	CPr_b: function() {
+		var i = Z80._r.a;
+		i -= Z80._r.b;
+		Z80._r.f |= 0x40;
+		if(!(i & 255)) Z80._r.f |= 0x80;
+		if(i < 0) Z80._r.f |= 0x10;
+		Z80._r.m = 1; Z80._r.t = 4;
+	}
+
+	NOP: function() {
+		Z80._r.m = 1; Z80._r.t = 4;
+	}
+};
+
+Z80Flag = {
+	Bits:{
+		Zero:0,
+		Ope:0,
+		HalfCarry:0,
+		Carry:0
+	},
+
+	Raw: function(){
+		ret = 0
+		if(Z80Flag.Bits.Zero){
+			ret |= 0x80
+		}
+		if(Z80Flag.Bits.Ope){
+			ret |= 0x40
+		}
+		if(Z80Flag.Bits.HalfCarry){
+			ret |= 0x20
+		}
+		if(Z80Flag.Bits.Carry){
+			ret |= 0x10
+		}
+		return ret
+	}
+
+	IssueZero: function(lastValue) {
+		if( !(lastValue & 0xFF) ){
+			Z80Flag.Bits.Zero = 1
+		}
+	}
+
+	IssueCarry: function(lastValue) {
+		if( lastValue > 0xFF ){
+			Z80Flag.Bits.Carry = 1
+		}
 	}
 };
