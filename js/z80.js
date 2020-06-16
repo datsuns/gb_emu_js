b9c836360440d57de1d67d688d3c1ad8afada879
datsuns <the.skeleton7@gmail.com>
2020-06-16 10:22:19 +0900

    add z80.js

---
+++ js/z80.js
@@ -0,0 +1,27 @@
+
+Z80 = {
+	_clock: {
+		m:0,
+		t:0
+	},
+
+	_r: {
+		// 8-bit regs
+		a:0,
+		b:0,
+		c:0,
+		d:0,
+		e:0,
+		h:0,
+		l:0,
+		f:0,
+
+		// 16-bit regs
+		pc:0,
+		sp:0,
+
+		// clocks for last inst
+		m:0,
+		t:0
+	}
+};
