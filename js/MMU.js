fd773043bc956fab9bfbff2c99bb0088ad75bc49
datsuns <the.skeleton7@gmail.com>
2020-06-17 09:02:57 +0900

    add memmap comment

--- js/MMU.js
+++ js/MMU.js
@@ -1,4 +1,19 @@
+// [0000-3FFF] Cartridge ROM, bank 0
+//   [0000-00FF] BIOS
+//   [0100-014F] Cartridge header
+// [4000-7FFF] Cartridge ROM, other banks
+// [8000-9FFF] Graphics RAM
+// [A000-BFFF] Cartridge (External) RAM
+// [C000-DFFF] Working RAM
+// [E000-FDFF] Working RAM (shadow)
+// [FE00-FE9F] Graphics: sprite information
+// [FF00-FF7F] Memory-mapped I/O
+// [FF80-FFFF] Zero-page RAM
+
 MMU = {
+	_inbios: 1,
+
+
 	rb: function(addr){},
 	rw: function(addr){},
 
